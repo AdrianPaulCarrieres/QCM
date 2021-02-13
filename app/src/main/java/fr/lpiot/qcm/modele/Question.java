ffc2b3be72a1a8bd4f4ba4a3d7bd1d1522713b68
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2021-02-13 15:02:18 +0100

    feat(ajout modèle question)

---
+++ app/src/main/java/fr/lpiot/qcm/modele/Question.java
@@ -0,0 +1,39 @@
+package fr.lpiot.qcm.modele;
+
+import java.util.ArrayList;
+
+public class Question {
+    protected String categorie;
+    protected String difficulte;
+    protected ArrayList<Reponse> listeReponses;
+
+    public Question(String categorie, String difficulte) {
+        this.categorie = categorie;
+        this.difficulte = difficulte;
+        listeReponses = new ArrayList<>;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public String getDifficulte() {
+        return difficulte;
+    }
+
+    public void setDifficulte(String difficulte) {
+        this.difficulte = difficulte;
+    }
+
+    public ArrayList<Reponse> getListeReponses() {
+        return listeReponses;
+    }
+
+    public void setListeReponses(ArrayList<Reponse> listeReponses) {
+        this.listeReponses = listeReponses;
+    }
+}
