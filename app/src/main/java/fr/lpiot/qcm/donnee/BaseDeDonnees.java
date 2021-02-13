7ee611dd738b0771e005dd930752aeb3c90b1c9b
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2021-02-13 10:24:32 +0100

    fix(renommage name/password -> nom/mot_de_passe pour rester constant :))

--- app/src/main/java/fr/lpiot/qcm/donnee/BaseDeDonnees.java
+++ app/src/main/java/fr/lpiot/qcm/donnee/BaseDeDonnees.java
@@ -36,10 +36,10 @@ public class BaseDeDonnees extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_TABLE = "create table utilisateur(id_utilisateur INTEGER PRIMARY KEY, name TEXT, password TEXT)";
+        String CREATE_TABLE = "create table utilisateur(id_utilisateur INTEGER PRIMARY KEY, nom TEXT, mot_de_passe TEXT)";
         db.execSQL(CREATE_TABLE);
 
-        String INSERT_TEST_USER = "insert into utilisateur(id_usitilisateur, name, password) VALUES('1', 'test', 'test')";
+        String INSERT_TEST_USER = "insert into utilisateur(id_utilisateur, nom, mot_de_passe) VALUES('1', 'test', 'test')";
         db.execSQL(INSERT_TEST_USER);
     }
 
