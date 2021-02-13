53b3d137a2ac16958b078054ac365051f989fa84
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2021-02-13 11:24:21 +0100

    feat(ajout button click)

--- app/src/main/java/fr/lpiot/qcm/LoginActivity.java
+++ app/src/main/java/fr/lpiot/qcm/LoginActivity.java
@@ -3,12 +3,54 @@ package fr.lpiot.qcm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import fr.lpiot.qcm.donnee.BaseDeDonnees;
+import fr.lpiot.qcm.donnee.UtilisateurDAO;
+import fr.lpiot.qcm.modele.Utilisateur;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //View
+    protected TextView textViewIdentifiant = findViewById(R.id.identifiant);
+    protected TextView textViewMotDePasse = findViewById(R.id.mdp);
+
+    //UtilisateurDAO
+    protected UtilisateurDAO accesseurUtilisateur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //BaseDeDonnees.getInstance(getApplicationContext());
+
+        //accesseurUtilisateur = UtilisateurDAO.getInstance();
+    }
+
+    private void buttonClick(View v){
+        String nom = textViewIdentifiant.getText().toString();
+        String motDePasse = textViewMotDePasse.getText().toString();
+
+        Utilisateur utilisateur = accesseurUtilisateur.chercherUtilisateurParNom(nom);
+
+        if(utilisateur == null) {
+            afficherToast("Mauvais identifiant");
+        }
+        else if(utilisateur.getMotDePasse().equals(motDePasse)){
+            afficherToast("Utilisateur connecté");
+        } else {
+            afficherToast("Mauvais mot de passe");
+        }
+    }
+
+
+    private void afficherToast(String message){
+        Toast t = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        t.show();
+        Log.i("login", "toasted message : " + message);
     }
 }
